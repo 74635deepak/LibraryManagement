4900e85f730fb7d2dcd3c6cc0ff3ffae4fba3320
74635deepak <deepaknikhare30@gmail.com>
2024-12-14 21:13:26 +0530

    Service Completed

--- src/main/java/com/example/LibraryManagement/LibServiceImpl.java
+++ src/main/java/com/example/LibraryManagement/LibServiceImpl.java
@@ -2,39 +2,47 @@ package com.example.LibraryManagement;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibServiceImpl implements LibService{
+	
+	@Autowired
+	private LibRepository libRepository;
 
 	@Override
 	public Book addBook(Book book) {
-		// TODO Auto-generated method stub
-		return null;
+		Book savedBook= libRepository.save(book);
+		return savedBook;
 	}
 
 	@Override
 	public Book getBookById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Book book =libRepository.findById(id).orElseThrow(()->new RuntimeException());
+		return book;
 	}
 
 	@Override
 	public List<Book> getAllBooks() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Book> books = libRepository.findAll();
+		return books;
 	}
 
 	@Override
 	public Book updateBook(Book book, Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Book recBook =libRepository.findById(id).orElseThrow(()->new RuntimeException());
+		recBook.setName(book.getName());
+		recBook.setAuthor(book.getAuthor());
+		recBook.setAbout(book.getAbout());
+		
+		Book sBook=libRepository.save(recBook);
+		return sBook;
 	}
 
 	@Override
 	public void deleteBook(Integer id) {
-		// TODO Auto-generated method stub
-		
+		libRepository.deleteById(id);
 	}
 
 }
